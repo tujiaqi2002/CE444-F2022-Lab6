fe311fefc600c0173c8b40858d4671cc8b750041
Tusu <321756623tu@gmail.com>
2022-11-23 00:44:38 -0500

    final version

--- tests/app_test.py
+++ tests/app_test.py
@@ -12,10 +12,11 @@ def client():
     BASE_DIR = Path(__file__).resolve().parent.parent
     app.config["TESTING"] = True
     app.config["DATABASE"] = BASE_DIR.joinpath(TEST_DB)
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{BASE_DIR.joinpath(TEST_DB)}"
 
-    init_db()  # setup
+    db.create_all()  # setup
     yield app.test_client()  # tests run here
-    init_db()  # teardown
+    db.drop_all()  # teardown
 
 
 def login(client, username, password):
@@ -91,3 +92,14 @@ def client():
     db.create_all()  # setup
     yield app.test_client()  # tests run here
     db.drop_all()  # teardown
+
+
+def test_delete_message(client):
+    """Ensure the messages are being deleted"""
+    rv = client.get("/delete/1")
+    data = json.loads(rv.data)
+    assert data["status"] == 0
+    login(client, app.config["USERNAME"], app.config["PASSWORD"])
+    rv = client.get("/delete/1")
+    data = json.loads(rv.data)
+    assert data["status"] == 1
